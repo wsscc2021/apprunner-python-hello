acea556453f456aa8008206ff9943b0c74a00fbb
Donggeun Lee <piou987@gmail.com>
2021-12-03 17:38:05 +0900

    Init project

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+import os
+
+PORT = 8080
+MESSAGE = "Hello, " + "Park" + "!"
+print("Message: '" + MESSAGE + "'")
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def root():
+  print("Handling web request. Returning message.")
+  result = MESSAGE.encode("utf-8")
+  return result
+
+
+if __name__ == "__main__":
+  app.run(debug=True, host="0.0.0.0", port=PORT)
